bbef0d0160042acc983aacd6e5ec9ae0d5187c10
jdeino <josh.deino91@yahoo.com>
2023-03-12 10:41:26 -0700

    added functions to js file

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
-// Arrays & For Loops
 var topics = ["HTML", "CSS" , "Git" , "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
 
-/* 
-var topic = "HTML";
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic() {
+    if (topics[0] === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopics === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopics === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+
+console.log("Here are topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
